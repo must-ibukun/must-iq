2e0b928a23cdf209c2919a14f944027ac7feac46
= <=>
2026-04-06 19:55:31 +0100

    update package

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@must-iq/shared-types', '@must-iq/db', '@must-iq/config'],
+  transpilePackages: ['@must-iq/shared-types'],
   experimental: {
     serverActions: { allowedOrigins: ['localhost:3000'] },
   },
